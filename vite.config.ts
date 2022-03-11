4ed2a85cdef47f40ee59e2607e4cd475dc54d20e
T. Kameyama <tkamenoko@vivaldi.net>
2022-03-12 00:27:09 +0900

    add:  optimizeDeps config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import linaria from '@linaria/rollup';
-import path from 'path';
-import { defineConfig } from 'vite';
+import linaria from "@linaria/rollup";
+import path from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [{ ...linaria(), enforce: 'post' }],
+  plugins: [{ ...linaria(), enforce: "post" }],
   resolve: {
-    alias: { '@': path.resolve('src') },
+    alias: { "@": path.resolve("src") },
+  },
+  optimizeDeps: {
+    include: ["@linaria/core", "@linaria/react", "polished"],
   },
 });
